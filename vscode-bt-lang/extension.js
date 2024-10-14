e55e873f359ee923b2cc8f3ef26325563d0c012c
bt-lang <46769985@qq.com>
2024-10-14 17:40:34 +0800

    修改扩展

--- vscode-bt-lang/extension.js
+++ vscode-bt-lang/extension.js
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const { execFile } = require('child_process');
+const path = require('path');
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -17,7 +19,7 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('bt-lang.highlight', function () {
+	const highlight = vscode.commands.registerCommand('bt-lang.highlight', function () {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
@@ -33,7 +35,29 @@ function activate(context) {
 			}
 		});
 	});
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(highlight);
+
+	const run = vscode.commands.registerCommand('extension.runBTScript', () => {
+        // 获取 bt.exe 的路径
+        const exePath = path.join(context.extensionPath, 'bt.exe'); // 替换为你的 bt.exe 路径
+
+        // 获取当前工作区的第一个文件夹的路径
+        const workspaceFolders = vscode.workspace.workspaceFolders;
+        if (!workspaceFolders) {
+            vscode.window.showErrorMessage("No workspace folder is open.");
+            return;
+        }
+        // 运行 bt.exe
+        execFile(exePath, [], (error, stdout, stderr) => {
+            if (error) {
+                vscode.window.showErrorMessage(`Error: ${stderr}`);
+                return;
+            }
+            vscode.window.showInformationMessage(`Output: ${stdout}`);
+        });
+    });
+
+    context.subscriptions.push(run);
 }
 
 // This method is called when your extension is deactivated
